f6837e9a06aed6a1c65867678c609cdc0831222a
Jeff Skelton <jeff@jeffskelton.net>
2017-07-22 22:43:51 -0700

    import config JSON file and tweak export code

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,23 +3,25 @@ import * as path from 'path';
 import * as Sequelize from 'sequelize';
 const basename  = path.basename(module.filename);
 const env       = process.env.NODE_ENV || 'development';
-import { config } from '../config/config';
+import * as config from '../config/config.json';
 
-let db:any;
+let db:any = {};
+let sequelize:any = null;
 
 if (config[env].use_env_variable) {
-  var sequelize = new Sequelize(process.env[config[env].use_env_variable]);
+  sequelize = new Sequelize(process.env[config[env].use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config[env].database, config[env].username, config[env].password, config[env]);
+  sequelize = new Sequelize(config[env].database, config[env].username, config[env].password, config[env]);
 }
-
+console.log('here');
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.ts');
   })
   .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
+    console.log(file);
+    let model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
@@ -32,4 +34,4 @@ Object.keys(db).forEach(function(modelName) {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export { db };
