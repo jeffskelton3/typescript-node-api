44cf2ca57dfc651e50bbf7d10703e4251f41e032
Jeff Skelton <jeff@jeffskelton.net>
2017-08-13 21:58:22 -0700

    hack out a couple framework design comments/goals for later.

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,11 +1,25 @@
 import Controller from './controller';
 import UsersService from "../services/users.service";
+// import {Controller, Get} from 'Typo';
 
+/*
+* Middleware that effects all endpoints in the controller.
+* @Controller({
+*   url: '/users',
+*   cors: {
+*       methods: '*',
+*       domains: '*'
+*   },
+*   before: [someMiddlewareFunc], // happens before the route is hit. Validations, auth, etc.
+*   after: [someOtherMiddlewareFunc] // happens after the route is done
+* });
+* */
 class UsersController extends Controller{
     constructor() {
         super();
     }
 
+    //@Get() inherits the properties of the @Controller and appends or overrides parameters as appropriate.
     public async getUsers(req, res) {
         try {
             const users = await UsersService.FindAll();
@@ -15,6 +29,15 @@ class UsersController extends Controller{
         }
     }
 
+    /*
+    * @Post({
+    *   url: '/create' <= will be '/users/create',
+    *   cors: {
+    *       domains: ['mydomain.com']
+    *   },
+    *   before: [middlewareFunctionSpecificToThisRoute]
+    * });
+    * */
     public async createUser(req, res) {
         try {
             const created = await UsersService.Create(req.body);
